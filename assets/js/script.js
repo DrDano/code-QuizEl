f7e21d12c1afad196e170df5ece95064e6040392
DrDano <danielharned@gmail.com>
2021-11-23 08:57:34 -0800

    adding structure to pseudocode and replacing high score anchor with button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 // create question bank with answers and a key to provide location of correct answer
+
 // when user clicks button, display the first question
 // display questions randomly from bank of questions
-// when user clicks the correct answer, move on to next question
-// when user clicks the wrong answer, move on to next question and remove 5 seconds from their timer
+// when user clicks the correct answer screen flashes green and they move on to next question
+// when user clicks the wrong answer the screen flashes red and they move on to next question and remove 5 seconds from their timer
+
 // when the user answers all questions the quiz is over
 // when the timer reaches 0 the quiz is over
 // when the quiz is over, display a form where they can enter their initials next to their high score.
+
 // store the user's highest score in an object with their initials as the key
 // store all high scores in JSON format in localStorage for retrieval whenever the user clicks on "view high scores"
